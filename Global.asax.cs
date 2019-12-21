acbd0dd428088f2edcc6afbce34255af63d748cb
FarshidKia <ninafarshidkia@gmail.com>
2019-12-21 17:09:34 +0300

    Global.asax.cs e new AuthorizeAttribute eklendi ve Authentication tüm uygulama seviyesine çıkartıldı.

--- Global.asax.cs
+++ Global.asax.cs
@@ -11,6 +11,7 @@ namespace ofis_ise
     {
         protected void Application_Start()
         {
+            GlobalFilters.Filters.Add(new AuthorizeAttribute());
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
