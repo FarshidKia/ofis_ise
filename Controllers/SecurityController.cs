d0a4ebce0b177ff73b6747622f158dcedf236852
FarshidKia <ninafarshidkia@gmail.com>
2019-12-21 16:41:52 +0300

    Authentication, Login işlemleri uygulandı.

---
+++ Controllers/SecurityController.cs
@@ -0,0 +1,44 @@
+﻿using ofis_ise.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ofis_ise.Controllers
+{
+    [AllowAnonymous]
+    public class SecurityController : Controller
+    {
+        // GET: Security
+        ofissEntities db = new ofissEntities();
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(Kullanici kullanici)
+        {
+            var kullaniciInDb = db.Kullanici.FirstOrDefault(x => x.Ad == kullanici.Ad && x.Sifre == kullanici.Sifre);
+            if (kullaniciInDb != null)
+            {
+                FormsAuthentication.SetAuthCookie(kullaniciInDb.Ad, false);
+                return RedirectToAction("Index", "Departman");
+            }
+            else
+            {
+                ViewBag.Mesaj = "Geçersiz Kullanıcı Adı Veya Geçersiz Şifre Girdiniz! Lütfen Bilgilerinizi Kontrol ediniz!";
+                return View();
+            }
+            //return View();
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
